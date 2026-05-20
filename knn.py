cb7b4c0e1a41e61db6e1027a42038573db8991e1
Kd3Us <jules.sorrentino@gmail.com>
2026-05-20 14:39:18 +0200

    feat: add euclidean distance private method

--- knn.py
+++ knn.py
@@ -1,3 +1,5 @@
+import math
+
 from data_loader import load_normalized_data
 
 
@@ -12,9 +14,16 @@ class KNN:
 		self.X_train = X
 		self.Y_train = Y
 
+	def _euclidean_distance(self, point_a, point_b):
+		squared_sum = 0
+		for i in range(len(point_a)):
+			squared_sum += (point_a[i] - point_b[i]) ** 2
+		return math.sqrt(squared_sum)
+
 
 if __name__ == "__main__":
 	X_normalized, Y, standard_scaler_object = load_normalized_data(file_path="bienetre.csv")
 	knn_object = KNN(n_neighbors=5)
 	knn_object.fit(X_normalized, Y)
-	print("Fit done")
+	distance = knn_object._euclidean_distance(X_normalized[0], X_normalized[1])
+	print(f"Distance between point 0 and point 1 : {distance}")
